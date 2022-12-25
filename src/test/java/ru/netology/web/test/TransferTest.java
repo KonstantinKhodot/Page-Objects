9e87368b3edd2105d1dc25fca331b6e9fd690cdc
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-12-25 21:12:55 +0300

    first commit

---
+++ src/test/java/ru/netology/web/test/TransferTest.java
@@ -0,0 +1,30 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards1() {
+        open("http://localhost:9999/");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards2() {
+        open("http://localhost:9999/");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+}
