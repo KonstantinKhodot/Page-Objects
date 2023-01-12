2379e0c38673b40ddab912f7bbf7c238e36bdf14
KonstantinKhodot <Khodot.konstantin@mail.ru>
2023-01-12 11:56:20 +0300

    correction

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -3,38 +3,38 @@ package ru.netology.web.page;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import lombok.val;
+import ru.netology.web.data.DataHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
 
+    private final String balanceStart = "баланс: ";
+
+    private final String balanceFinish = " р.";
+
     private SelenideElement heading = $("[data-test-id=dashboard]");
 
     private ElementsCollection cards = $$(".list__item div");
-    private final String balanceStart = "баланс: ";
-    private final String balanceFinish = " р.";
 
     public DashboardPage() {
         heading.shouldBe(visible);
     }
 
-    public String getCardBalance(int index) {
-        return $$("ul li[class=list__item] div").get(index).getOwnText();
-    }
-
-    public void addMoneyToCard(int index) {
-        $$("ul li[class=list__item] div button").get(index).click();
-    }
-
-    public int getFirstCardBalance() {
-        val text = cards.first().text();
+    public int getCardBalance(DataHelper.CardInfo cardInfo) {
+        var text = cards.findBy(text(cardInfo.getCardNumber().substring(15))).getText();
         return extractBalance(text);
+    }
+
+    public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
+        cards.findBy(attribute("data-test-id", cardInfo.getTestId())).$("button").click();
+        return new TransferPage();
     }
 
     private int extractBalance(String text) {
